a446e67205124e98565fc04e48a603a6fa998459
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-11-28 09:22:44 -0800

    start resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth.js');
+
+const resolvers = {
+    Query:{
+
+    },
+    Mutation: {
+        
+    }
+
+}
+
+module.exports = resolvers;
